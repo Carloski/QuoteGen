94528c113495ed36bf396d06762e719674417b0e
Carlos <carlosmtamayo@gmail.com>
2019-11-14 11:08:45 -0500

    Initial Commit
    Added files to gihub

---
+++ js/quotes.js
@@ -0,0 +1,58 @@
+let quotes = [
+
+    {
+        phrase: 'I’m just always looking forward. I spend very little time, looking backward',
+        source: '"Gary Vaynerchuk"'
+    },
+    {
+        phrase: 'Success is a lousy teacher. It seduces smart people into thinking they can’t lose.',
+        source: 'Bill Gates'
+    },
+    {
+        phrase: 'If you are born poor it’s not your mistake, but if you die poor it’s your mistake',
+        source: 'Bill Gates'
+    },
+    {
+        phrase: 'Be nice to nerds. Chances are you’ll end up working for one.',
+        source: 'Tony Robbins'
+    },
+    {
+        phrase: 'Most people fail in life because they major in minor things.',
+        source: 'Tony Robbins'
+    },
+    {
+        phrase: 'The only thing that’s keeping you from getting what you want is the story you keep telling yourself.',
+        source: 'Tony Robbins'
+    },
+    {
+        phrase: 'Success in life is the result of good judgment. Good judgment is usually the result of experience. Experience is usually the result of bad judgment.',
+        source: 'Tony Robbins'
+    },
+    {
+        phrase: 'Anybody can make history. Only a great man can write it.',
+        source: 'Oscar Wilde',
+        citation: 'The Critic as Artist, Part I, p. 49',
+        year: 1891
+    },
+
+    {
+        phrase: 'Great men are not born great, they grow great . . .',
+        source: 'Mario Puzo, The Godfather'
+    },
+    {
+        phrase: 'Be yourself; everyone else is already taken.',
+        source: 'Oscar Wilde',
+        year: 1954
+    },
+
+    {
+        phrase: 'Prayer must never be answered: if it is, it ceases to be prayer and becomes correspondence.',
+        source: 'Oscar Wilde',
+        citation: 'Quoted by Alvin Redman in The Epigrams of Oscar Wilde',
+        year: 1952
+    }
+
+ 
+];
+
+console.log(quotes);
